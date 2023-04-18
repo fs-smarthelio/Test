0e7b662314190f181fe26695cdec9965cf2e1c54
Wojciech Sromek <wojciech@smarthelio.com>
2023-04-18 18:10:01 +0530

    proper export

--- smarthelio_shared/__init__.py
+++ smarthelio_shared/__init__.py
@@ -8,3 +8,4 @@ from .sh_utility import melt_multiindex_to_simple_index
 from .transpose_ghi_to_poa import transposition_model
 from .get_system_info_from_metadb import SystemInfoMetadataAPI
 from .ghi_from_visualcrossing import VisualCrossingGHI
+from .plant_provider import PlantProvider

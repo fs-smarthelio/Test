ea37d2332888bd4a0101e698da7f9b3f27ec68f7
BhavyaD <bhavya@smarthelio.com>
2023-04-18 16:41:33 +0530

    added a new class to fetch GHI data from VC

--- smarthelio_shared/__init__.py
+++ smarthelio_shared/__init__.py
@@ -7,3 +7,4 @@ from .sh_utility import melt_multiindex_to_simple_index
 
 from .transpose_ghi_to_poa import transposition_model
 from .get_system_info_from_metadb import SystemInfoMetadataAPI
+from .ghi_from_visualcrossing import VisualCrossingGHI

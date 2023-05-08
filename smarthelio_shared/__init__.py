d017f66001932b586a6fddd49e9ced67ac8a6426
BhavyaD <bhavya@smarthelio.com>
2023-05-08 17:00:08 +0530

    minor changes

--- smarthelio_shared/__init__.py
+++ smarthelio_shared/__init__.py
@@ -5,7 +5,11 @@ from .sh_utility import split_tag
 from .sh_utility import timestream_transform
 from .sh_utility import melt_multiindex_to_simple_index
 
+from .filtering import *
+from .plant_provider import PlantProvider
+from .add_multi_index_level import add_index_curve_level
+from .estimate_Tamb import estimate_air_temperature
+from .estimate_Tmod import estimate_module_temperature
 from .transpose_ghi_to_poa import transposition_model
 from .get_system_info_from_metadb import SystemInfoMetadataAPI
 from .ghi_from_visualcrossing import VisualCrossingGHI
-from .plant_provider import PlantProvider
--- smarthelio_shared/estimate_Tmod.py
+++ smarthelio_shared/estimate_Tmod.py
@@ -1,4 +1,8 @@
+"""This file calculates Module Temperature using Irradiance and Ambient
+temperature."""
+import pandas as pd
 import math
+from add_multi_index_level import add_index_curve_level
 
 
 def estimate_tmod(df_in, Tamb_col='Tamb_csky', G_col='G'):
@@ -27,3 +31,44 @@ def estimate_tmod(df_in, Tamb_col='Tamb_csky', G_col='G'):
     df['Tmod'] = irradiance * (math.exp(a)) + air_temperature
 
     return df.Tmod
+
+
+def estimate_module_temperature(df, irr_str, tamb_str='Tamb'):
+    """
+    This function helps to estimate Module Temperature using Sandia
+    Module Temperature Model.
+
+     # Sandia Module Temperature Model
+
+    # https://pvpmc.sandia.gov/modeling-steps/2-dc-module-iv/module-
+    temperature/sandia-module-temperature-model/ )
+
+    Parameters
+    ----------
+    meteo_data: Pandas Dataframe
+        Dataframe with Irradiance values
+    irr_str: string
+        Irradiance column name
+    tamb_str: string
+        Ambient temperature column name
+
+    Returns
+    meteo_data : Pandas dataframe
+        dataframe with estimated module temperature column
+    """
+    # extracting Irradiance and air temperature
+    irradiance = df.xs(irr_str, axis=1, level='curve')
+    air_temperature = df.xs(tamb_str, axis=1, level='curve')
+
+    # Module type: Glass/cell/polymer sheet # Mount type: Open rack
+    a = -3.56
+    b = -.0750
+    # estimating module temp using the relationship between Irradiance and Tamb
+    est_mod_temp = irradiance * (math.exp(a)) + air_temperature
+    # converting est_mod_temp into multi-index dataframe
+    est_mod_temp.columns = add_index_curve_level(est_mod_temp.columns, 'Tmod')
+    # adding module temperature to meteo_data df
+    df = pd.concat([df, est_mod_temp], axis=1)
+
+    return df
+

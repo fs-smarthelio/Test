5c1b11cf7225e4c47d612f0cd6a8f2681fd515fb
BhavyaD <bhavya@smarthelio.com>
2023-05-26 14:35:14 +0530

    updated to 1.6.5

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='smarthelio_shared',
     packages=find_packages(include=['smarthelio_shared']),
     install_requires=['pandas', 'requests', 'pvlib', 'numpy', 'rdtools', 'wheel', 'pytz'],
-    version='1.6.4',
+    version='1.6.5',
     description='SmartHelio shared',
     author='SmartHelio',
     license='MIT',

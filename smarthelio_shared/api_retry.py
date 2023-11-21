39d23a589a9363d34f2055af0dc51ddec353ae55
Wojciech Sromek <wojciech@smarthelio.com>
2023-11-21 14:43:07 +0100

    fix(): Fix breaking change in urllib Retry constructor

--- smarthelio_shared/api_retry.py
+++ smarthelio_shared/api_retry.py
@@ -14,7 +14,7 @@ def retry_session(url, retries=3, backoff_factor=3, codes_to_retry_on=CODES_TO_R
         total=retries,
         backoff_factor=backoff_factor,
         status_forcelist=codes_to_retry_on,
-        method_whitelist=methods_to_retry_on
+        allowed_methods=methods_to_retry_on
     )
     adapter = HTTPAdapter(max_retries=retry)
     session.mount(url, adapter)

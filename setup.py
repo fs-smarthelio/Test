530844dbb04d4335b2b974ddc7a12eba36f6f73f
Wojciech Sromek <wojciech@smarthelio.com>
2023-11-22 14:22:40 +0100

    Make sure that we always install urllib3 < 2.0.0, plus fixed constructor args

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 setup(
     name='smarthelio_shared',
     packages=find_packages(include=['smarthelio_shared']),
-    install_requires=['urllib3>=2.0.0', 'pandas', 'requests', 'pvlib', 'numpy', 'rdtools', 'wheel', 'pytz'],
+    install_requires=['urllib3<2.0.0', 'pandas', 'requests', 'pvlib', 'numpy', 'rdtools', 'wheel', 'pytz'],
     version='1.11.4',
     description='SmartHelio shared',
     author='SmartHelio',

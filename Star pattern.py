876866e784b8c11055148d959e6337620a4c52ca
Franky Saxena <franky@smarthelio.com>
2023-11-23 17:43:08 +0530

    Did little bit changes in the format of the code for Branch `Franky`.

--- Star pattern.py
+++ Star pattern.py
@@ -1,4 +1,4 @@
 n = int(input("Enter the number to show the pattern: "))
 for i in range(n):
-    print(' '*(n - i-1), end='')
-    print('*'*(2*i+1))
+    print(' '*(n - i - 1), end='')
+    print('*'*(2*i + 1))

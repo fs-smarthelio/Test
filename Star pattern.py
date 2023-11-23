bc5d8cbe9dae239e0fbf3b21d7a61458b4c7a51d
Franky Saxena <franky@smarthelio.com>
2023-11-23 17:38:38 +0530

    Change the pattern for the `Franky` Branch.

--- Star pattern.py
+++ Star pattern.py
@@ -1,2 +1,4 @@
-for i in range(int(input("Enter the number to show the pattern: "))):
-    print('*'*i)
+n = int(input("Enter the number to show the pattern: "))
+for i in range(n):
+    print(' '*(n - i-1), end='')
+    print('*'*(2*i+1))
